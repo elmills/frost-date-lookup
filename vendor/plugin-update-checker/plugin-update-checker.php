a7774c36c0a14ffda8f065f7f12cb278a150806c
Everette Mills <elmills@elmills.net>
2025-04-05 13:01:05 -0400

    Finished ready to release 1.0.0

--- vendor/plugin-update-checker/plugin-update-checker.php
+++ vendor/plugin-update-checker/plugin-update-checker.php
@@ -1,42 +1,10 @@
 <?php
-/*
- * Plugin Update Checker
- * 
- * This file is part of the Plugin Update Checker library.
- * 
- * The Plugin Update Checker library is a simple way to add automatic updates to your WordPress plugins and themes.
- * 
- * For more information, visit: https://github.com/YahnisElsts/plugin-update-checker
+/**
+ * Plugin Update Checker Library 5.5
+ * http://w-shadow.com/
+ *
+ * Copyright 2024 Janis Elsts
+ * Released under the MIT license. See license.txt for details.
  */
 
-if ( ! class_exists( 'Puc_v4_Factory' ) ) {
-    class Puc_v4_Factory {
-        public static function buildUpdateChecker( $metadataUrl, $pluginFile, $slug = null ) {
-            return new Puc_v4_Plugin_UpdateChecker( $metadataUrl, $pluginFile, $slug );
-        }
-    }
-}
-
-if ( ! class_exists( 'Puc_v4_Plugin_UpdateChecker' ) ) {
-    class Puc_v4_Plugin_UpdateChecker {
-        private $metadataUrl;
-        private $pluginFile;
-        private $slug;
-
-        public function __construct( $metadataUrl, $pluginFile, $slug = null ) {
-            $this->metadataUrl = $metadataUrl;
-            $this->pluginFile = $pluginFile;
-            $this->slug = $slug ?: plugin_basename( $pluginFile );
-            add_action( 'upgrader_process_complete', array( $this, 'checkForUpdates' ), 10, 2 );
-        }
-
-        public function checkForUpdates( $upgrader, $hook_extra ) {
-            // Logic to check for updates
-        }
-
-        // Additional methods for update checking
-    }
-}
-
-// Additional classes and methods for the update checker can be added here.
-?>
+require dirname(__FILE__) . '/load-v5p5.php';
